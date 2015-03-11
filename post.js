0838e1064130fbc95af488aab1102d37d47c9cb0
edenmanny <emmanuelbernard@Emmanuels-MacBook-Pro.local>
2015-03-11 18:38:59 -0400

    fist git
    this is my app for social date site

---
+++ post.js
@@ -0,0 +1,40 @@
+$(document).on('pagebeforeshow', '#login', function(){  
+        $(document).on('click', '#submit', function() { // catch the form's submit event
+        if($('#username').val().length > 0 && $('#txt-password').val().length > 0){
+            // Send data to server through ajax call
+            // action is functionality we want to call and outputJSON is our data
+                $.ajax({url: 'check.php',
+                    data: {action : 'login', formData : $('#check-user').serialize()}, // Convert a form to a JSON string representation
+                        type: 'post',                   
+                    async: true,
+                    beforeSend: function() {
+                        // This callback function will trigger before data is sent
+                        $.mobile.showPageLoadingMsg(true); // This will show ajax spinner
+                    },
+                    complete: function() {
+                        // This callback function will trigger on data sent/received complete
+                        $.mobile.hidePageLoadingMsg(); // This will hide ajax spinner
+                    },
+                    success: function (result) {
+                            resultObject.formSubmitionResult = result;
+                                        $.mobile.changePage("#second");
+                    },
+                    error: function (request,error) {
+                        // This callback function will trigger on unsuccessful action                
+                        alert('Network error has occurred please try again!');
+                    }
+                });                   
+        } else {
+            alert('Please fill all nececery fields');
+        }           
+            return false; // cancel original event to prevent form submitting
+        });    
+});
+
+$(document).on('pagebeforeshow', '#second', function(){     
+    $('#second [data-role="content"]').append('This is a result of form submition: ' + resultObject.formSubmitionResult);  
+});
+
+var resultObject = {
+    formSubmitionResult : null  
+}
